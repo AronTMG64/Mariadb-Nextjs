752de7566a1a5e9d5cb9b7c486ecc11e781df677
AronTMG64 <a.grankulla@optistud.fi>
2023-11-15 14:25:07 +0200

    init

--- src/app/api/delete/route.ts
+++ src/app/api/delete/route.ts
@@ -2,6 +2,6 @@ import { db } from "@/utils/db";
 
 export async function DELETE(req: Request, res: Response) {
   const body = await req.json();
-  db.query("DELETE FROM users WHERE id = ?", [body.id]);
+  db.query('DELETE FROM users WHERE id = ?', [body.id]);
   return Response.redirect('http://localhost:3000/admin');
 };
